9b3fcf36ef37839d555d350e616bc949d9749972
Barak Kaldess <ibarakkaldess@gmail.com>
2022-09-22 18:28:27 +0300

    adding atlasDB

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  //dbURL: process.env.MONGO_URI,
+  dbURL: 'mongodb+srv://betterAdmin:betterAdmin@cluster0.bkuyxmp.mongodb.net/?retryWrites=true&w=majority',
+  // dbURL: process.env.MONGO_URI,
 }
