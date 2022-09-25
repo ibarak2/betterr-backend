bd1cff1635ae2388b225a320298cc7e654c103d9
Barak Kaldess <ibarakkaldess@gmail.com>
2022-09-25 16:40:17 +0300

    socket cancel order flow

--- services/socket.service.js
+++ services/socket.service.js
@@ -8,9 +8,10 @@ function setupSocketAPI(http) {
       origin: "*",
     },
   })
+
   gIo.on("connection", (socket) => {
     logger.info(`New connected socket [id: ${socket.id}]`)
-    socket.on("disconnect", (socket) => {
+    socket.on("disconnect", () => {
       logger.info(`Socket disconnected [id: ${socket.id}]`)
     })
     socket.on("chat-set-topic", (topic) => {
@@ -24,6 +25,7 @@ function setupSocketAPI(http) {
       socket.join(topic)
       socket.myTopic = topic
     })
+
     socket.on("chat-send-msg", (msg) => {
       logger.info(
         `New chat msg from socket [id: ${socket.id}], emitting to topic ${socket.myTopic}`
@@ -33,21 +35,37 @@ function setupSocketAPI(http) {
       // emits only to sockets in the same room
       gIo.to(socket.myTopic).emit("chat-add-msg", msg)
     })
+
     socket.on("user-watch", (userId) => {
       logger.info(
         `user-watch from socket [id: ${socket.id}], on user ${userId}`
       )
       socket.join("watching:" + userId)
     })
+
     socket.on("set-user-socket", (userId) => {
+      console.log("my loggedin");
       logger.info(
         `Setting socket.userId = ${userId} for socket [id: ${socket.id}]`
       )
       socket.userId = userId
     })
+
     socket.on("unset-user-socket", () => {
       logger.info(`Removing socket.userId for socket [id: ${socket.id}]`)
       delete socket.userId
+    })
+
+    socket.on("new-order-request", (sellerId) => {
+      logger.info(`New order sent to ${sellerId}`)
+
+      emitToUser({ type: "new-order-recieved", data: "New Order Recieved", userId: sellerId })
+    })
+
+    socket.on("order-cancelled", (miniOrder) => {
+      logger.info(`Order cancelled.`)
+
+      emitToUser({ type: "on-order-cancelled", data: { txt: "Order Cancelled", orderId: miniOrder._id }, userId: miniOrder.userId })
     })
   })
 }
@@ -66,6 +84,7 @@ async function emitToUser({ type, data, userId }) {
       `Emiting event: ${type} to user: ${userId} socket [id: ${socket.id}]`
     )
     socket.emit(type, data)
+
   } else {
     logger.info(`No active socket for user: ${userId}`)
     // _printSockets()
@@ -80,6 +99,7 @@ async function broadcast({ type, data, room = null, userId }) {
 
   logger.info(`Broadcasting event: ${type}`)
   const excludedSocket = await _getUserSocket(userId)
+
   if (room && excludedSocket) {
     logger.info(`Broadcast to room ${room} excluding user: ${userId}`)
     excludedSocket.broadcast.to(room).emit(type, data)
@@ -100,6 +120,7 @@ async function _getUserSocket(userId) {
   const socket = sockets.find((s) => s.userId === userId)
   return socket
 }
+
 async function _getAllSockets() {
   // return all Socket instances
   const sockets = await gIo.fetchSockets()
@@ -111,6 +132,7 @@ async function _printSockets() {
   console.log(`Sockets: (count: ${sockets.length}):`)
   sockets.forEach(_printSocket)
 }
+
 function _printSocket(socket) {
   console.log(`Socket - socketId: ${socket.id} userId: ${socket.userId}`)
 }
