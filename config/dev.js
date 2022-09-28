ae251c6f69f1ad3505f825c9ba05d0c40b177adb
Barak Kaldess <ibarakkaldess@gmail.com>
2022-09-28 12:53:36 +0300

    gitignore

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb://localhost:27017',
+  // dbURL: 'mongodb://localhost:27017',
+  dbURL: 'mongodb+srv://betterr_admin:hBQaDahOapxEC8t2@cluster0.0nke0uc.mongodb.net/?retryWrites=true&w=majority',
 }
